a243b6ed14f10d909efd3927d9b91afe4b5cb56f
anibal <aebs1982@gmail.com>
2025-06-30 11:38:09 -0300

    atualizando as views

---
+++ views/acoes_view.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+class TelaAcoes:
+    def __init__(self, controller):
+        self.controller - controller
+        self.root = controller.root
+        self.frame = tk.Frame(self.root, bg="#DCE6F1")
+
+        sel.rootconfigure(bg="#DCE6F1")
+        self.root.atributes('-fullscreen', True)
+
+def mostrar(self):
+    self.root.title("ClinControl")
+    self.frame.pack(padx=20, pady=20)
+    var_opcao = tk.StringVar (Value="")
+
+    tk.Button(self.frame, text="Cadastrar Turista", width=30, comand=self.controller.abrir_cadastro_turista).pack(pady=5)
+    tk.Button(self.frame, text="Reservar", width=30).pack(pady=5)
+    tk.Button(self.frame, text="Reservar", width=30).pack(pady=5)
+    tk.Button(self.frame, text="Reservar", width=30).pack(pady=5)
+    tk.Button(self.frame, text="Sair", width=30, comand=self.root.quit).pack(pady=5)
